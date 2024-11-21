71318a57d5d390ddc48f199c814a76a78df74ed8
WolfangFF <128604702+WolfangFF@users.noreply.github.com>
2024-11-21 11:23:43 -0500

    Update tips.js

--- routes/tips.js
+++ routes/tips.js
@@ -1,1 +1,32 @@
+const express = require('express');
+const router = express.Router()
+const fs = require('fs');
+const app = express();
+const axios = require('axios')
+const cheerio = require('cheerio');
+const noblox = require('noblox.js')
+const socketService = require('../utils/socketService');
+const wait = require('wait');
+const { channel } = require('diagnostics_channel');
+const currentDate = new Date();
+const crypto = require('crypto');
+const res = require('express/lib/response');
+const uuidv4 = require('uuid').v4
+const { getRandomInt, generateRandomHash, round } = require('../utils/randomHash.js');
+const path = require('path');
+const { connect,
+  changeUserBalance,
+  getUserData,
+  checkUserIdExists,
+  addnewUser
+ } = require('../utils/dbChange');
+const { info } = require('console');
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/tips.html'));
+      
+    })
+  
+  
+  
+    module.exports = router
